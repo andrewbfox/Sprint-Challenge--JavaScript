885710dc623b9290c072c12ca95a12353feb919d
Andrew Fox <andrewbfox@gmail.com>
2019-06-14 16:32:32 -0400

    Modified functions.js to clean up callbacks code and remove commenting-out of closures section

--- challenges/functions.js
+++ challenges/functions.js
@@ -8,8 +8,7 @@
 */
 function consume(param1, param2, callback) {
   return callback(param1, param2);
-  
-}
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -17,15 +16,15 @@ function consume(param1, param2, callback) {
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 function add(num1, num2) {
-  return (num1 + num2)
-}
+  return num1 + num2;
+};
 
 function multiply(num1, num2) {
-  return num1 * num2
+  return num1 * num2;
 };
 
 function greeting(firstName, lastName) {
-  return `Hello ${firstName} ${lastName}, nice to meet you!`
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
 };
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
@@ -33,12 +32,11 @@ console.log(consume(2,2,add)); // 4
 console.log(consume(10,16,multiply)); // 160
 console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
-/*
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: nestedFunction can access the variable 'internal' because it is declared outside of nestedFunction. nestedFunction can look outside itself for its context.
+// Explanation:  nestedFunction can access the variable 'internal' because it is declared outside of nestedFunction. nestedFunction can look outside itself for its context.
 
 
 const external = "I'm outside the function";
@@ -52,4 +50,4 @@ function myFunction() {
   };
   nestedFunction();
 }
-myFunction();*/
+myFunction();
